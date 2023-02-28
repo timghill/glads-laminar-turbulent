e173d52f583e1b2f14e1127da0419eeadc297def
tghill <tghill@uwaterloo.ca>
2023-02-28 09:01:10 -0800

    Add locals

--- glads/data/kan_l_melt/plot_KAN_moulin.m
+++ glads/data/kan_l_melt/plot_KAN_moulin.m
@@ -1,34 +1,47 @@
 % function melt=KAN_moulin(time, pin, dmesh, ii_moulin, catchmap, ra)
 
-moulins = load('../synthetic_moulins_KAN/moulins_KAN_025.txt');
+moulins = load('../moulins/moulins_068.txt');
 ii_moulin = moulins(:, 1);
-catchmap = load('../synthetic_moulins_KAN/catchment_map_KAN_025.txt');
+catchmap = load('../moulins/catchment_map_068.txt');
 meshes = load('../mesh/mesh.mat');
 dmesh = meshes.meshes{4};
 
-pin.bed_elevation = @(xy, tt) 0*xy(:, 1) + 350;
-pin.ice_thickness = @(xy, tt) 6*(sqrt(xy(:, 1) + 5000) - sqrt(5000)) + 390 - 350;
+addpath('../topo_x_squared_para/')
+addpath('../functions/')
+%pin.bed_elevation = @(xy, tt) 0*xy(:, 1) + 350;
+%pin.ice_thickness = @(xy, tt) 6*(sqrt(xy(:, 1) + 5000) - sqrt(5000)) + 390 - 350;
 
-tt = 86400*(0:365) + 5*365*86400;
+pin.bed_elevation = @(xy, t) bed_elevation_flat(xy, t);
+pin.ice_thickness = @(xy, t) ice_thickness_flat(xy, t);
 
+tt = 86400*(0:0.25:365) + 100*365*86400;
 melt = zeros(dmesh.tri.n_nodes, length(tt));
 for ii=1:length(tt)
-    melt(:, ii) = KAN_moulin(tt(ii), pin, dmesh, ii_moulin, catchmap, 0);
+    melt(:, ii) = KAN_moulin_seasonal(tt(ii), pin, dmesh, ii_moulin, catchmap);
 end
 
-t_plt = tt/86400 - tt(1);
+dt= tt(2) - tt(1);
+total_minputs = sum(melt(:))
+area = 100e3 * 25e3;
+mean_melt = total_minputs/area * dt
+
+
+t_plt = tt/86400 - tt(1)/86400;
 m_plt = 1:length(ii_moulin);
 [xx, yy] = meshgrid(t_plt, m_plt);
 
 figure
 pcolor(xx, yy, melt(ii_moulin, :))
-% caxis([0, 10])
+% caxis([0, 40])
 shading flat
 cb = colorbar;
 cb.Label.String = 'Moulin input (m^3 s^{-1})';
 xlabel('Day of year')
 ylabel('Moulin')
-% cmocean('amp')
+cmocean('amp')
 print('KAN_moulin_inputs', '-dpng', '-r600')
 
 max(melt(ii_moulin, :), [], 2)
+
+figure
+plot(t_plt, melt(ii_moulin(17), :))
