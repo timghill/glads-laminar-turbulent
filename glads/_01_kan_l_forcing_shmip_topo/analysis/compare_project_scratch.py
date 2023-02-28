14208d35dbbafa97c2e361f44a170c185a0d0634
Tim Hill <tha111@sfu.ca>
2023-02-28 15:46:32 -0800

    S figures

---
+++ glads/_01_kan_l_forcing_shmip_topo/analysis/compare_project_scratch.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import netCDF4 as nc
+
+project = nc.Dataset('../RUN/output_001_steady.nc');
+scratch = nc.Dataset('/home/tghill/subglacial-emulator/glads/seasonal_KAN/RUN/output_301_steady.nc')
+
+# fig, (ax1, ax2) = plt.subplots()
+
+"""
+    variables(dimensions): float64 rho_w(), float64 rho_i(), float64 g_grav(), float64 L_fusion(), float64 c_w(), float64 c_t_c(), float64 hour(), float64 day(), float64 year(), float64 n_glen(), float64 creep_const_s(), float64 creep_const_s_soft(), float64 creep_const_s_trans(), float64 creep_const_c(), float64 l_bed(), float64 l_c(), float64 h_bed(), float64 alpha_c(), float64 beta_c(), float64 cond_c(), float64 nu(), float64 omega(), float64 cond_s(cond), float64 alpha_s(cond), float64 beta_s(cond)
+"""
+
+print(project['para/cond_s'][:].data)
+print(scratch['para/cond_s'][:].data)
+
+print(project['para/l_c'][:].data)
+print(scratch['para/l_c'][:].data)
+
+print(project['para/cond_c'][:].data)
+print(scratch['para/cond_c'][:].data)
+
+print(project['para/alpha_c'][:].data)
+print(scratch['para/alpha_c'][:].data)
+
+proj_bed = project['bed'][:].data
+scra_bed = scratch['bed'][:].data
+
+print(proj_bed - scra_bed)
+
+proj_thick = project['thick'][:].data
+scra_thick = scratch['thick'][:].data
+
+print(proj_thick - scra_thick)
+
+print(project['S_channel'][:].data.T.sum(axis=0))
+print(scratch['S_channel'][:].data.T.sum(axis=0))
+
+
---
+++ glads/_01_kan_l_forcing_shmip_topo/analysis/plot_S.py
@@ -0,0 +1,25 @@
+import netCDF4 as nc
+import numpy as np
+from matplotlib import pyplot as plt
+
+cases = [1, 2, 3, 4, 5]
+fname_pattern = '../RUN/output_%03d_seasonal.nc'
+figname = 'project_channels.png'
+
+# cases = [301, 302, 303]
+# fname_pattern = '/home/tghill/scratch/subglacial-emulator/glads/seasonal_KAN/RUN/output_%03d_seasonal.nc'
+# figname = 'scratch_channels.png'
+
+fig, ax = plt.subplots()
+for i in range(len(cases)):
+    fname = fname_pattern % cases[i]
+    out = nc.Dataset(fname)
+    S = out['S_channel'][:].data.T
+    time = out['time'][:].data
+    time = (time - time[0])/86400/365
+
+    ax.plot(time, np.sum(S, axis=0))
+
+fig.savefig(figname, dpi=600)
+plt.show()
+
