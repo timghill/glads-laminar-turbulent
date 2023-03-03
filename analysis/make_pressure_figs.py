360570b68127557599a62cd2c9bdbcb9bd39d5c1
Tim Hill <tha111@sfu.ca>
2023-03-03 14:44:04 -0800

    Driver

--- analysis/make_pressure_figs.py
+++ analysis/make_pressure_figs.py
@@ -4,6 +4,8 @@ Call plot_pressure_maps_timeseries.py to make pressure figures
 
 """
 
+from matplotlib import pyplot as plt
+
 from plot_pressure_maps_timeseries import plot_pressure_maps_timeseries
 
 ## Case 00: Flat topo, SHMIP forcing
@@ -13,15 +15,18 @@ figname = '00_pressure_seasonal.png'
 fig_00 = plot_pressure_maps_timeseries(fnames, figname)
 
 ## Case 01: Flat topo, KAN_L forcing
+KAN_tslice = 569
 cases = [101, 102, 104, 104, 105]
 pattern = '../glads/01_kan_l_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
 figname = '01_pressure_seasonal.png'
-fig_01 = plot_pressure_maps_timeseries(fnames, figname)
+fig_01 = plot_pressure_maps_timeseries(fnames, figname, tslice=KAN_tslice, Qmin=1)
 
 ## Case 03: Trough topo, KAN_L forcing
 cases = [101, 102, 104, 104, 105]
 pattern = '../glads/03_kan_l_forcing_synth_topo/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
 figname = '03_pressure_seasonal.png'
-fig_03 = plot_pressure_maps_timeseries(fnames, figname)
+fig_03 = plot_pressure_maps_timeseries(fnames, figname, tslice=KAN_tslice, Qmin=1)
+
+plt.show()
