1783e2d63fda87fdd3ebcc5f8ab05f4445d5645e
Tim Hill <tha111@sfu.ca>
2023-03-07 12:58:06 -0800

    Temp ylims

--- analysis/plot_pressure_maps_timeseries.py
+++ analysis/plot_pressure_maps_timeseries.py
@@ -228,7 +228,7 @@ def plot_pressure_maps_timeseries(fnames, figname, tslice=defaults.tslice,
             
             ax_right.set_ylabel('Temp ($^\circ$C)')
             ax_right.set_ylim([0, 12])
-            ax_right.set_yticks([0, 5, 10, 15])
+            ax_right.set_yticks([0, 4, 8, 12])
             timeax.set_zorder(3)
             ax_right.set_zorder(2)
             timeax.patch.set_visible(False)
