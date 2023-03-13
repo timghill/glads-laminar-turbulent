4338d68a0c70dbd53d0aace7998c3e613df981b8
tghill <tghill@uwaterloo.ca>
2023-03-13 11:51:59 -0700

    Plot script

--- analysis/make_pressure_figs.py
+++ analysis/make_pressure_figs.py
@@ -13,6 +13,7 @@ t_ticklabels = ['4', '6', '8', '10']
 t_lim = [t_ticks[0], t_ticks[-1]]
 t_xlabel = 'Month'
 
+"""
 ## Case 00: Flat topo, SHMIP forcing
 cases = [1, 2, 3, 4, 5]
 fnames = ['../glads/00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
@@ -26,8 +27,16 @@ fnames = ['../glads/00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%cas
 figname = '00_pressure_seasonal_200.png'
 fig_00 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, melt_forcing='SHMIP',
     t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12])
+"""
 
+## Case 00a: Flat topo, scaled SHMIP forcing
+cases = [1, 2, 3, 4, 5]
+fnames = ['../glads/00a_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
+figname = '00_pressure_seasonal_scaled_melt.png'
+fig_00 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, melt_forcing='SHMIP',
+    t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12])
 
+"""
 ## Case 01: Flat topo, KAN_L forcing
 KAN_tslice = 569
 cases = [101, 102, 103, 104, 105]
@@ -62,5 +71,5 @@ fnames = [pattern % caseid for caseid in cases]
 figname = '03_pressure_seasonal.png'
 fig_03 = plot_pressure_maps_timeseries(fnames, figname, tslice=KAN_tslice, Qmin=1, melt_forcing='KAN',
     t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim)
-
+"""
 plt.show()
