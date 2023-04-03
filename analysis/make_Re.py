892b17a172d9fa093b096fcc0a3a5b54b0b24e0e
Tim Hill <tha111@sfu.ca>
2023-04-03 16:32:33 -0700

    New tslice

--- analysis/make_Re.py
+++ analysis/make_Re.py
@@ -23,6 +23,8 @@ figname = '00_Re_seasonal_SHMIP.png'
 #     t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12])
 fig_00 = plot_Re(fnames, figname, Qmin=10, Qmax=200, Re_ylim=(0, 8e3))
 
+
+
 ## Case 00.2: Flat topo, SHMIP forcing, consistent para
 cases = [201, 202, 203, 204, 205]
 fnames = ['../glads/00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
@@ -35,6 +37,14 @@ cases = [1, 2, 3, 4, 5]
 fnames = ['../glads/00a_shimp_adj_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
 figname = '00_Re_seasonal.png'
 fig_00 = plot_Re(fnames, figname, Qmin=1, Qmax=100, Re_ylim=(0, 4e3))
+
+
+## Case 00a: Flat topo, scaled SHMIP forcing
+cases = [1, 2, 3, 4, 5]
+fnames = ['../glads/00a_shimp_adj_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
+figname = '00_Re_seasonal_day152.png'
+fig_00 = plot_Re(fnames, figname, Qmin=1, Qmax=100, Re_ylim=(0, 4e3), tslice=517)
+
 
 ## Case 01: Flat topo, KAN_L forcing
 cases = [201, 202, 203, 204, 205]
