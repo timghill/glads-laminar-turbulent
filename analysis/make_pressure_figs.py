fc9144c9c837f96fa8b095f8be1c792f8f0f8584
Tim Hill <tha111@sfu.ca>
2023-04-06 09:16:53 -0700

    Add figs

--- analysis/make_pressure_figs.py
+++ analysis/make_pressure_figs.py
@@ -15,75 +15,84 @@ t_ticklabels = ['May', 'July', 'Sep', 'Nov']
 t_lim = [t_ticks[0], t_ticks[-1]]
 t_xlabel = 'Month'
 
-"""
-## Case 00: Flat topo, SHMIP forcing
+## Case 00: Flat topo, synthetic forcing
 cases = [1, 2, 3, 4, 5]
-fnames = ['../glads/00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
-figname = '00_pressure_seasonal_SHMIP.png'
-fig_00 = plot_pressure_maps_timeseries(fnames, figname, melt_forcing='SHMIP', Qmin=10, Qmax=200,
-    t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12])
-
-## Case 00.2: Flat topo, SHMIP forcing, consistent para
-cases = [201, 202, 203, 204, 205]
-fnames = ['../glads/00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
-figname = '00_presure_seasonal_SHMIP_200.png'
-fig_00 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='SHMIP',
-    t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12],
-    ff_ylim=[0,1.75], ff_yticks=[0, 0.5, 1, 1.5])
-
-
-## Case 00a: Flat topo, scaled SHMIP forcing
-cases = [1, 2, 3, 4, 5]
-fnames = ['../glads/00a_shimp_adj_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
+fnames = ['../glads/00_synth_forcing/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
 figname = '00_pressure_seasonal.png'
-fig_00 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, Qmax=100, melt_forcing='SHMIPadj',
+fig_00 = plot_pressure_maps_timeseries(fnames, figname, melt_forcing='SHMIPadj', Qmin=1, Qmax=100,
     t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12])
-"""
+
+## Case 00a: Flat topo, standard SHMIP forcing
+cases = [1, 2, 3, 4, 5]
+fnames = ['../glads/00a_shmip_forcing/RUN/output_%03d_seasonal.nc'%caseid for caseid in cases]
+figname = '00_pressure_seasonal_shmip_forcing.png'
+fig_00 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='SHMIP',
+    t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1 + 3/12, 1 + 9/12])
 
 ## Case 01: Flat topo, KAN_L forcing
-cases = [201, 202, 203, 204, 205]
-pattern = '../glads/01_kan_l_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'
+cases = [1, 2, 3, 4, 5]
+pattern = '../glads/01_kan_forcing/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
 figname = '01_pressure_seasonal.png'
 fig_01 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, Qmax=100, melt_forcing='KAN',
      t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
      ff_ylim=[0, 1.75], ff_yticks=[0, 0.5, 1, 1.5])
+
 """
-## Case 01b: Flat topo, KAN_L increased forcing
+## Case 01a: KAN increased forcing
 cases = [1, 2, 3, 4, 5]
-pattern = '../glads/01a_kan_adj_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'
+pattern = '../glads/01a_kan_adj_forcing/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
-figname ='01b_pressure_seasonal.png'
-fig_01 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='KAN',
+figname ='01a_pressure_seasonal.png'
+fig_01 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='KANadj',
      t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
      ff_ylim=[0, 1.75], ff_yticks=[0, 0.5, 1, 1.5])
 
-## Case 02a: Trough topo, SHMIP forcing
+## Case 01b: Flat topo, KAN_L forcing, reduced e_v
 cases = [1, 2, 3, 4, 5]
-pattern = '../glads/02a_shmip_forcing_valley_topo/RUN/output_%03d_seasonal.nc'
+pattern = '../glads/01b_kan_forcing_ev/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
-figname = '02a_pressure_seasonal.png'
-fig_02 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=100, melt_forcing='SHMIP',
+figname = '01b_pressure_seasonal_ev.png'
+fig_01 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, Qmax=100, melt_forcing='KAN',
+     t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
+     ff_ylim=[0, 1.75], ff_yticks=[0, 0.5, 1, 1.5])
+"""
+
+## Case 02a: Trough topo, synthetic forcing
+cases = [1, 2, 3, 4, 5]
+pattern = '../glads/02a_synth_forcing_trough/RUN/output_%03d_seasonal.nc'
+fnames = [pattern % caseid for caseid in cases]
+figname = '02a_pressure_seasonal_trough.png'
+fig_02 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, Qmax=100, melt_forcing='SHMIPadj',
     t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1+3/12, 1+9/12],
     ff_ylim=[0,1.75], ff_yticks=[0, 0.5, 1, 1.5])
 
-## Case 02: Valley topo, SHMIP forcing
+## Case 02b: Valley topo, synthetic
 cases = [1, 2, 3, 4, 5]
-pattern = '../glads/02a_shmip_forcing_valley_topo/RUN/output_%03d_seasonal.nc'
+pattern = '../glads/02b_synth_forcing_valley/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
-figname = '02_pressure_seasonal_valley.png'
-fig_02 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='SHMIP',
+figname = '02b_pressure_seasonal_valley.png'
+fig_02 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='SHMIPadj',
     t_ticklabels=t_ticklabels[:-1], t_xlabel=t_xlabel, t_ticks=t_ticks[:-1], t_lim=[1+3/12, 1+9/12],
     ff_ylim=[0,1.75], ff_yticks=[0, 0.5, 1, 1.5])
 
 
-## Case 03: Trough topo, KAN_L forcing
-cases = [101, 102, 103, 104, 105]
-pattern = '../glads/03_kan_l_forcing_synth_topo/RUN/output_%03d_seasonal.nc'
+## Case 03a: Trough topo, KAN forcing
+cases = [1, 2, 3, 4, 5]
+pattern = '../glads/03a_kan_forcing_trough/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
-figname = '03_pressure_seasonal.png'
+figname = '03a_pressure_seasonal_trough.png'
 fig_03 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, melt_forcing='KAN',
     t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
     ff_ylim=[0,1.75], ff_yticks=[0, 0.5, 1, 1.5])
-"""
-plt.show()
+
+## Case 03b: Valley topo, KAN forcing
+cases = [1, 2, 3, 4, 5]
+pattern = '../glads/03b_kan_forcing_valley/RUN/output_%03d_seasonal.nc'
+fnames = [pattern % caseid for caseid in cases]
+figname = '03b_pressure_seasonal_valley.png'
+fig_03 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, melt_forcing='KAN',
+    t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
+    ff_ylim=[0,1.75], ff_yticks=[0, 0.5, 1, 1.5])
+
+
