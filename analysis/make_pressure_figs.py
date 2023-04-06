837b858fc51f36e55da97f68d54821f5d30b29c3
Tim Hill <tha111@sfu.ca>
2023-04-06 13:50:00 -0700

    1b early fig

--- analysis/make_pressure_figs.py
+++ analysis/make_pressure_figs.py
@@ -49,16 +49,15 @@ figname ='01a_pressure_seasonal.png'
 fig_01 = plot_pressure_maps_timeseries(fnames, figname, Qmin=10, Qmax=200, melt_forcing='KANadj',
      t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
      ff_ylim=[0, 1.75], ff_yticks=[0, 0.5, 1, 1.5])
-
+"""
 ## Case 01b: Flat topo, KAN_L forcing, reduced e_v
-cases = [1, 2, 3, 4, 5]
+cases = [1, 2, 3, 3, 3]
 pattern = '../glads/01b_kan_forcing_ev/RUN/output_%03d_seasonal.nc'
 fnames = [pattern % caseid for caseid in cases]
 figname = '01b_pressure_seasonal_ev.png'
 fig_01 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, Qmax=100, melt_forcing='KAN',
      t_ticklabels=t_ticklabels, t_xlabel=t_xlabel, t_ticks=t_ticks, t_lim=t_lim,
      ff_ylim=[0, 1.75], ff_yticks=[0, 0.5, 1, 1.5])
-"""
 
 ## Case 02a: Trough topo, synthetic forcing
 cases = [1, 2, 3, 4, 5]
