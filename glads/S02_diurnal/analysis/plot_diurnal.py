4ea063afe61415dcbb060a069beafa111ac9175b
timghill <tim_hill_2@sfu.ca>
2023-04-27 14:02:15 -0700

    Update

---
+++ glads/S02_diurnal/analysis/plot_diurnal.py
@@ -0,0 +1,61 @@
+"""
+Plot diurnal simulations
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+import netCDF4 as nc
+
+def _get_ff(ncout):
+    phi = ncout['phi'][:].data.T
+    rhow = 1000
+    g = 9.8
+    bed = np.vstack(ncout['bed'][:].data)
+    phi_elev = rhow*g*bed
+    pw = phi - phi_elev
+
+    N = ncout['N'][:].data.T
+    ff = pw/(N + pw)
+
+    return ff
+
+def diurnal(cases, figname):
+    fig, ax = plt.subplots(figsize=(8, 4))
+
+    colors = np.array([[0.579, 0.677, 0.781, 1],
+                    [0.199, 0.328, 0.492, 1],
+                    [0.500, 0.500, 0.500, 1],
+                    [0.929, 0.835, 0.408, 1],
+                    [0.859, 0.683, 0.275, 1]])
+
+    for i in cases:
+        fname_steady = '../RUN/output_%03d_steady.nc' % i
+        fname_diurnal = '../RUN/output_%03d_seasonal.nc' % i
+        out_steady = nc.Dataset(fname_steady, 'r')
+        out_diurnal = nc.Dataset(fname_diurnal, 'r')
+
+        ff_steady = _get_ff(out_steady)
+        ff_diurnal = _get_ff(out_diurnal)
+
+        nodes = out_diurnal['nodes'][:].data.T
+        node_mask = np.abs(nodes[:, 0] - 30e3)<=2.5e3
+
+        tt_steady = out_steady['time'][:].data
+        tt_diurnal = out_diurnal['time'][:].data
+
+        tt_steady = (tt_steady - tt_steady[-1])/86400
+        tt_diurnal = (tt_diurnal - tt_diurnal[0])/86400
+        ax.plot(tt_steady, np.mean(ff_steady[node_mask], axis=0), color=colors[i-1])
+        ax.plot(tt_diurnal, np.mean(ff_diurnal[node_mask], axis=0), color=colors[i-1])
+
+    ax.grid(linestyle=':', linewidth=0.5)
+    ax.set_xlim([-5, 10])
+
+
+if __name__=='__main__':
+    cases = [1, 2, 3, 4, 5]
+
+    figname = 'diurnals.png'
+
+    diurnal(cases, figname)
+    plt.show()
