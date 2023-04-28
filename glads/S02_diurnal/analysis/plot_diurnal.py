7630ed992332b0821c017c3917ac6ee36094c846
Tim Hill <tha111@sfu.ca>
2023-04-27 21:03:42 -0700

    Legend

--- glads/S02_diurnal/analysis/plot_diurnal.py
+++ glads/S02_diurnal/analysis/plot_diurnal.py
@@ -27,6 +27,7 @@ def diurnal(cases, figname):
                     [0.500, 0.500, 0.500, 1],
                     [0.929, 0.835, 0.408, 1],
                     [0.859, 0.683, 0.275, 1]])
+    labels = ['Turbulent 5/4', 'Turbulent 3/2', 'Laminar', 'Transition 5/4', 'Transition 3/2']
 
     for j in range(len(cases)):
         fname_steady = '../RUN/output_%03d_steady.nc' % cases[j]
@@ -46,13 +47,12 @@ def diurnal(cases, figname):
         tt_steady = (tt_steady - tt_steady[-1])/86400
         tt_diurnal = (tt_diurnal - tt_diurnal[0])/86400
         ax.plot(tt_steady, np.mean(ff_steady[node_mask], axis=0) - np.mean(ff_steady[node_mask], axis=0)[-1], color=colors[j])
-        ax.plot(tt_diurnal, np.mean(ff_diurnal[node_mask], axis=0) -np.mean(ff_steady[node_mask], axis=0)[-1], color=colors[j])
+        ax.plot(tt_diurnal, np.mean(ff_diurnal[node_mask], axis=0) -np.mean(ff_steady[node_mask], axis=0)[-1], color=colors[j], label=labels[j])
 
     ax.grid(linestyle=':', linewidth=0.5)
     ax.set_xlim([-0.5, 5.5])
     ax.set_ylim([-0.15, 0.15])
-    ax.legend(['Turbulent 5/4', 'Turbulent 3/2', 'Laminar', 'Transition 5/4', 'Transition 3/2'],
-        bbox_to_anchor=(-0.1, 1.02, 1.2, 0.1), mode='expand', ncol=5, frameon=False)
+    ax.legend(bbox_to_anchor=(-0.1, 1.02, 1.2, 0.1), mode='expand', ncol=5, frameon=False)
     ax.set_xlabel('Days')
     ax.set_ylabel(r'$\Delta p_{\rm{w}}/p_{\rm{i}}$')
 
