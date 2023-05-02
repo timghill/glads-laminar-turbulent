b85088bda7a779b2136501a4f5af1f4f17ac9e07
timghill <tim_hill_2@sfu.ca>
2023-05-02 10:45:23 -0700

    Moody diagram

--- analysis/plot_moody_scatter.py
+++ analysis/plot_moody_scatter.py
@@ -10,16 +10,10 @@ from scipy.optimize import newton
 from scipy import interpolate
 
 from matplotlib import pyplot as plt
+from matplotlib import ticker
 import netCDF4 as nc
 
 import defaults
-
-# pattern = '../glads/00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'
-# cases = [201, 202, 203, 204, 205]
-
-# pattern = '../glads/_00_shmip_forcing_shmip_topo/RUN/output_%03d_seasonal.nc'
-# cases = [1, 2, 3, 4, 5]
-
 
 models = ['Turbulent 5/4', 'Turbulent 3/2', 'Laminar', 'Transition 5/4', 'Transition 3/2']
 
@@ -40,7 +34,7 @@ def plot_moody(fnames, figname, colors=defaults.colors, omega=1/2000,
     eps_d_labels = [r'$10^{-3}$', r'$10^{-2}$', r'$10^{-1}$', r'$0.5$']
 
     # Reynolds number for computation
-    Re = np.logspace(3 + 0, 8, 101)
+    Re = np.logspace(3 + 0, 6, 101)
     Re_safe = Re.copy()
 
     # Compute friction factor as function of Re
@@ -55,7 +49,7 @@ def plot_moody(fnames, figname, colors=defaults.colors, omega=1/2000,
         friction = 1/ff**2
         darcy_friction[ii] = friction
 
-    Re_laminar = np.logspace(2, 3, 21)
+    Re_laminar = np.logspace(1, 3, 21)
     laminar_friction = 64/Re_laminar
 
     Re_transition = np.logspace(3, 3 + np.log10(3), 5)
@@ -65,13 +59,13 @@ def plot_moody(fnames, figname, colors=defaults.colors, omega=1/2000,
     ax_theory.loglog(Re, darcy_friction.T, color='k')
     ax_theory.loglog(Re_laminar, laminar_friction, color='k')
     ax_theory.loglog(Re_transition, laminar_transition, color='k', linestyle='--')
-    ax_theory.grid(linestyle=':')
+    ax_theory.grid(linestyle=':', linewidth=0.5)
 
     ax_theory.set_yticks(list(np.arange(1e-2, 1e-1, 1e-2)) + list(np.arange(1e-1, 1e0 + 1e-1, 1e-1)))
     ax_theory.set_ylim([1e-2, 1e0])
     ax_theory.set_xlim([Re_laminar[0], Re[-1]])
 
-    ax_theory.fill_betweenx([1e-2, 1e0], [1000, 1000], [3000, 3000], facecolor='grey', alpha=0.7, zorder=10)
+    ax_theory.fill_betweenx([1e-2, 1e0], [1000, 1000], [3000, 3000], facecolor='grey', alpha=0.7, zorder=0)
 
     ax_theory.set_xlabel(r'$\rm{Re} = \frac{VD}{\nu}$')
     ax_theory.set_ylabel(r'Friction factor $f_{\rm{D}}$', labelpad=0)# = \frac{h_{\rm{f}}}{\left(\frac{L}{D}\right) \frac{V^2}{2g}}$')
@@ -84,9 +78,10 @@ def plot_moody(fnames, figname, colors=defaults.colors, omega=1/2000,
     ax_right.set_yticklabels(eps_d_labels)
     ax_right.set_ylabel(r'Roughness $\frac{\epsilon}{D}$', labelpad=0)
 
-    ax_theory.text(1e2, 0.3, 'Laminar', rotation=-65, fontweight='bold', color='gray')
+    # ax_theory.text(1e2, 0.3, 'Laminar', rotation=-65, fontweight='bold', color='gray')
+    ax_theory.text(1.5e1, 1.2e-1, 'Laminar', rotation=0, fontweight='bold', color='gray')
     # ax_theory.text(2000, 0.6, 'Transition', rotation=0, fontweight='bold', color='gray', ha='center')
-    ax_theory.text(1e6, 0.5, 'Turbulent', rotation=0, fontweight='bold', color='gray')
+    ax_theory.text(1e4, 0.5, 'Turbulent', rotation=0, fontweight='bold', color='gray')
 
 
     for ii in range(len(fnames)):
@@ -135,35 +130,48 @@ def plot_moody(fnames, figname, colors=defaults.colors, omega=1/2000,
 
     ax_scatter.set_xscale('log')
     ax_scatter.set_yscale('log')
-    ax_scatter.set_xlim([1e1, 1e7])
+    ax_scatter.set_xlim([1e1, 1e6])
     ax_scatter.set_ylim([1e0, 1e2])
+    # ax_scatter.set_ylim([5e-1, 5e1])
+    ax_scatter.set_xticks([1e1, 1e2, 1e3, 1e4, 1e5, 1e6])
+    
 
-    ax_scatter.grid(linestyle=':', zorder=0)
+    ax_scatter.grid(linestyle=':', linewidth=0.5, zorder=0)
 
+    ## OPTIONAL: scaling for plotting model and empirical curves on the same axes
     # k_32 = 0.0189208879284245
     # eps = 0.1
     # # c = 1000*k_32**2/4/(np.log(eps/3.7)**2)
     # c = f_D
-    # print(f_D/k_32)
-    c = darcy_friction[2][-1]/f_32
-
+    # # print(f_D/k_32)
+    # c = darcy_friction[2][-1]/f_32
     # c = 64/Re[0] / f_lam
+    # 
+    # print(c)
+    # print(eps_d[-3])
+    # print(darcy_friction.shape)
+    # print(darcy_friction[-3]/c)
+    # ax_scatter.loglog(Re_safe, darcy_friction.T/c, color='k', linewidth=0.5)
+    # ax_scatter.loglog(Re_laminar, 64/Re_laminar/c, color='k', linewidth=0.5)
+    # ax_scatter.loglog(Re_transition, 64/Re_transition/c, color='k', linewidth=0.5, linestyle='--')
 
-    # print('c1:', c)
-    # print('c2:', c2)
 
     ax_scatter.set_xlabel(r'${\rm{Re}} = \frac{q}{\nu}$')
     # ax_scatter.set_ylabel(r'$f_{\rm{D}} = \frac{h^3 |\nabla \phi|}{\rho_{\rm{w}} \nu^2 {\rm{Re}}^2}$', fontsize=12)
-    ax_scatter.set_ylabel(r'Friction factor $f_{\rm{D}}$', labelpad=0)
+    ax_scatter.set_ylabel(r'Modelled friction factor $f_{\rm{D}}$', labelpad=0)
     # ax_scatter.legend(markerscale=5, loc='upper right', framealpha=1, edgecolor='none')
-    ax_scatter.text(-0.2, 1, 'b', transform=ax_scatter.transAxes, fontweight='bold')
-    print(c)
-    print(eps_d[-3])
-    print(darcy_friction.shape)
-    print(darcy_friction[-3]/c)
-    ax_scatter.loglog(Re_safe, darcy_friction.T/c, color='k', linewidth=0.5)
-    ax_scatter.loglog(Re_laminar, 64/Re_laminar/c, color='k', linewidth=0.5)
-    ax_scatter.loglog(Re_transition, 64/Re_transition/c, color='k', linewidth=0.5, linestyle='--')
+    # ax_scatter.text(-0.2, 1, 'b', transform=ax_scatter.transAxes, fontweight='bold')
+
+    ax_sctwin = ax_scatter.twinx()
+
+    ax_sctwin.loglog(Re_safe, darcy_friction.T, color='k', linewidth=0.5, zorder=0)
+    ax_sctwin.loglog(Re_laminar, 64/Re_laminar, color='k', linewidth=0.5, zorder=-1)
+    ax_sctwin.loglog(Re_transition, 64/Re_transition, color='k', linewidth=0.5, linestyle='--')
+    ax_sctwin.set_yticks(list(np.arange(1e-2, 1e-1, 1e-2)) + list(np.arange(1e-1, 1e0 + 1e-1, 1e-1)))
+    ax_sctwin.set_ylim([1e-2, 1e0])
+    ax_sctwin.set_ylabel(r'Empirical friction factor $f_{\rm{D}}$', labelpad=4)
+    ax_sctwin.set_zorder(ax_scatter.get_zorder() - 1)
+    ax_scatter.patch.set_visible(False)
 
 
     ax_scatter.legend(bbox_to_anchor=[0.1, 1.02, 0.8, 0.2],
@@ -182,23 +190,12 @@ def plot_moody(fnames, figname, colors=defaults.colors, omega=1/2000,
     ax_scatter.set_yticks(yticks)
 
 
-    # ax_scatter.text(1e1, 1e1, 'Laminar', rotation=-63, fontweight='bold', color='gray')
     ax_scatter.text(1.5e1, 1.2e1, 'Laminar', rotation=0, fontweight='bold', color='gray')
-    # ax_scatter.text(2000, 50, 'Transition', rotation=0, fontweight='bold', color='gray', ha='center')
-    ax_scatter.text(1e5, 50, 'Turbulent', rotation=0, fontweight='bold', color='gray')
+    ax_scatter.text(1e4, 50, 'Turbulent', rotation=0, fontweight='bold', color='gray')
 
 
-    fig_theory.subplots_adjust(wspace=0.45, bottom=0.15, left=0.125, right=0.875, top=0.95)
-    fig_scatter.subplots_adjust(wspace=0.45, bottom=0.125, left=0.125, right=0.97, top=0.85)
-
-    # ax_scatter.legend(bbox_to_anchor=[-0.125, 1.02, 1.125, 0.102], loc='lower left',
-    #     ncol=2, mode='expand', borderaxespad=0.05, frameon=False, borderpad=0,
-    #     markerscale=5)
-    # fig.subplots_adjust(wspace=0.6, bottom=0.2, left=0.1, right=0.95, top=0.8)
-
-
-    # ax_scatter.axhline(max_f_D, color='k', linewidth=1)
-    # plt.tight_layout()
+    fig_theory.subplots_adjust(wspace=0.45, bottom=0.15, left=0.125, right=0.85, top=0.95)
+    fig_scatter.subplots_adjust(wspace=0.45, bottom=0.125, left=0.125, right=0.85, top=0.85)
 
     fig_theory.savefig(fignames[0], dpi=600)
     fig_scatter.savefig(fignames[1], dpi=600)
