abc21fe89234404b491ec66621b83609f33a3fc6
Tim Hill <tha111@sfu.ca>
2023-08-15 16:25:14 -0700

    Move s01b steady

--- analysis/plot_steady.py
+++ analysis/plot_steady.py
@@ -283,7 +283,7 @@ if __name__=='__main__':
     ## Case S01b: Flat topo, synthetic forcing
     cases = [1, 2, 3, 4, 5]
     fnames = ['../glads/S01b_parameter_sensitivity/RUN/output_%03d_steady.nc'%caseid for caseid in cases]
-    figname = 'S01b_steady.png'
+    figname = 'figures/aux/S01b_steady.png'
     fig_00 = plot_pressure_maps_timeseries(fnames, figname, Qmin=1, Qmax=100)
 
 
