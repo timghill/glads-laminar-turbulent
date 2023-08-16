49ec5733323045bab544e583710867e476608a93
Tim Hill <tha111@sfu.ca>
2023-08-16 09:17:53 -0700

    Plot marine-terminating outlet

---
+++ glads/00c_synth_marine/run_models.m
@@ -0,0 +1,16 @@
+% Run laminar, turbulent, and transition transient models
+
+%% Laminar models
+% run_steady(5e-2, 0, 3, 2, 'laminar_steady.nc');
+% run_seasonal(5e-2, 0, 3, 2, 'laminar_steady.nc', 'laminar_seasonal.nc');
+%
+% Turbulent models
+k_turb = sqrt( 1.97e-6/(1/1000)*5e-2);
+% k_turb = 5e-3;
+run_steady(k_turb, 0, 5/4, 3/2, 'turbulent_steady.nc');
+run_seasonal(k_turb, 0, 5/4, 3/2, 'turbulent_steady_hard.nc', 'turbulent_seasonal_hard.nc');
+%
+
+% % Transition models
+% run_steady(5e-2, 1/1000, 3, 2, 'transition_steady_1.5.nc');
+% run_seasonal(5e-2, 1/1000, 3, 2, 'transition_steady_1.5.nc', 'transition_seasonal_1.5.nc');
