f28e4c1a11fa4ceb9aafc3422260fb1b071ce8fa
timghill <tghill@edu.uwaterloo.ca>
2023-10-16 19:22:21 -0400

    ISSM comparison

--- issm/00_synth_forcing/do_convert.m
+++ issm/00_synth_forcing/do_convert.m
@@ -1,5 +1,8 @@
+addpath('../');
+set_paths;
+
 ids = [1, 2, 3, 4, 5];
 for ii=1:length(ids)
-    fname = sprintf('RUN/output_%03d_steady.mat', ids(ii))
+    fname = sprintf('RUN/output_%03d.mat', ids(ii))
     convert_issm_outputs(fname);
 end
--- issm/00_synth_forcing/run_job.m
+++ issm/00_synth_forcing/run_job.m
@@ -19,6 +19,19 @@ config.e_v = 1e-4;
 
 config.omega = omega;
 
+if config.alpha_s<3 && config.omega==0
+    % Compute potential gradient for turbulent conductivity scaling
+    p_w_min = 40*910*9.81;
+    p_w_max = 1520*910*9.81;
+    gradphi = (p_w_max - p_w_min)/100e3;
+    omega = 1/2000;
+    nu = 1.79e-6;
+    h3 = nu/(omega)/config.k_s/gradphi;
+    k_s = config.k_s * h3^(1 - config.alpha_s/3) * gradphi^(2 - 3/2);
+    config.k_s = k_s;
+end
+
+
 % dmeshfile = '/home/tghill/scratch/laminar-turbulent/glads/data/mesh/mesh.mat';
 % dmeshfile = '/home/tghill/SFU-code/laminar-turbulent/glads/data/mesh/mesh.mat';
 dmeshfile = '../../glads/data/mesh/mesh.mat';
@@ -35,6 +48,6 @@ md=solve(md,'Transient');
 if ~isfolder('RUN/')
     mkdir('RUN/')
 end
-config.fname = sprintf('RUN/output_%03d_steady.mat', id);
+config.fname = sprintf('RUN/output_%03d.mat', id);
 save(config.fname, 'md');
 
