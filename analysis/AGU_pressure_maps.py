25513be16b4190a3e1abf8097d73065424a11ab6
tghill <tghill@uwaterloo.ca>
2023-12-05 20:06:12 -0800

    Update agu

--- analysis/AGU_pressure_maps.py
+++ analysis/AGU_pressure_maps.py
@@ -131,34 +131,34 @@ def plot_pressure_maps_timeseries(fnames, figname, tslice=defaults.tslice,
 
 
     ## Start the figure
-    fig = plt.figure(figsize=figsize)
+    fig, mapax = plt.figure(figsize=figsize)
 
     # A global gridspec giving two columns to work with
-    global_gs = gridspec.GridSpec(1, 3, **gs_kwargs)
+    # global_gs = gridspec.GridSpec(1, 3, **gs_kwargs)
 
     # Left column: 3 timeseries panels and melt forcing
-    hratios = 100*np.ones(len(x_bands)+2)
-    hratios[0] = 8
-    gs_timeseries = gridspec.GridSpecFromSubplotSpec(len(x_bands) + 2, 1, 
-        subplot_spec=global_gs[:, 2], height_ratios=hratios)
+    # hratios = 100*np.ones(len(x_bands)+2)
+    # hratios[0] = 8
+    # gs_timeseries = gridspec.GridSpecFromSubplotSpec(len(x_bands) + 2, 1, 
+    #     subplot_spec=global_gs[:, 2], height_ratios=hratios)
 
     # Right column: 5 maps with space for colorbars
-    hratios = 100*np.ones(n_cases+2)
-    hratios[0] = 8
-    hratios[-1] = 150
-    gs_maps = gridspec.GridSpecFromSubplotSpec(n_cases+2, 2, 
-        subplot_spec=global_gs[:, 0], width_ratios=(100, 4), height_ratios=hratios,
-        hspace=0.1  , wspace=0.1)
+    # hratios = 100*np.ones(n_cases+2)
+    # hratios[0] = 8
+    # hratios[-1] = 150
+    # gs_maps = gridspec.GridSpecFromSubplotSpec(n_cases+2, 2, 
+    #     subplot_spec=global_gs[:, 0], width_ratios=(100, 4), height_ratios=hratios,
+    #     hspace=0.1  , wspace=0.1)
 
     # Initialize axes
-    axs_timeseries = np.array([fig.add_subplot(gs_timeseries[i+1, 0]) for i in range(len(x_bands) + 1)])
-    axs_maps = np.array([fig.add_subplot(gs_maps[i+1, 0]) for i in range(n_cases)])
-    ax_scatter = fig.add_subplot(gs_maps[-1, 0])
+    # axs_timeseries = np.array([fig.add_subplot(gs_timeseries[i+1, 0]) for i in range(len(x_bands) + 1)])
+    # axs_maps = np.array([fig.add_subplot(gs_maps[i+1, 0]) for i in range(n_cases)])
+    # ax_scatter = fig.add_subplot(gs_maps[-1, 0])
 
     # Set style for panel labels
-    time_alphabet = ['g', 'h', 'i', 'j']
-    map_alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
-    text_args = {'fontweight':'bold'}
+    # time_alphabet = ['g', 'h', 'i', 'j']
+    # map_alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
+    # text_args = {'fontweight':'bold'}
     # Start reading the data
     for ii in range(n_cases):
         fname = fnames[ii]
@@ -191,7 +191,7 @@ def plot_pressure_maps_timeseries(fnames, figname, tslice=defaults.tslice,
         mtri = Triangulation(nodes[:, 0]/1e3, nodes[:, 1]/1e3, connect)
         
         # Map panel
-        mapax = axs_maps[ii]
+        # mapax = axs_maps[ii]
         fcolor = mapax.tripcolor(mtri, ff[:, tslice], cmap=map_cmap, vmin=0, vmax=1)
         mapax.set_aspect('equal')
         mapax.set_xlim([0, 100])
@@ -206,121 +206,123 @@ def plot_pressure_maps_timeseries(fnames, figname, tslice=defaults.tslice,
         lc = gplt.plot_edge_data(nodes/1e3, connect_edge, Q[:, tslice],
             line_cmap, vmin=Qmin, vmax=Qmax)
         mapax.add_collection(lc)
+        mapax.set_xlabel('Distance from terminus (km)')
+        mapax.set_ylabel('Distance across (km)')
 
-        if ii<n_cases:
-            mapax.set_xticklabels([])
+        # if ii<n_cases:
+        #     mapax.set_xticklabels([])
         
-        mapax.text(0.95, 0.95, map_alphabet[ii], transform=mapax.transAxes,
-            va='top', ha='right', color='w', **text_args)
-        mapax.text(0.95, 0.05, labels[ii], transform=mapax.transAxes,
-            va='bottom', ha='right', fontsize=8, color='w')
-        xmid, ff_avg = helpers.weighted_width_average(nodes, ff[:, tslice], node_area)
+        # mapax.text(0.95, 0.95, map_alphabet[ii], transform=mapax.transAxes,
+        #     va='top', ha='right', color='w', **text_args)
+        # mapax.text(0.95, 0.05, labels[ii], transform=mapax.transAxes,
+        #     va='bottom', ha='right', fontsize=8, color='w')
+        # xmid, ff_avg = helpers.weighted_width_average(nodes, ff[:, tslice], node_area)
 
-        quantile_95 = lambda x: np.quantile(x, 0.95)
+        # quantile_95 = lambda x: np.quantile(x, 0.95)
 
-        if fill_between:
-            ax_scatter.fill_between(xmid/1e3, ff_lower, ff_upper, facecolor=colors[ii], alpha=0.33,
-                edgecolor=None, linewidth=linewidths[ii], zorder=zorders[ii],
-                linestyle=linestyles[ii])
-        ax_scatter.plot(xmid/1e3, ff_avg, color=colors[ii], label=labels[ii],
-            linewidth=lws[ii], linestyle=linestyles[ii],
-            zorder=zorders[ii])
-        ax_scatter.set_ylim(ff_ylim)
-        ax_scatter.set_yticks(ff_yticks)
-        ax_scatter.set_xlim([0, 100])
-        ax_scatter.grid(linestyle=':', linewidth=0.5)
+        # if fill_between:
+        #     ax_scatter.fill_between(xmid/1e3, ff_lower, ff_upper, facecolor=colors[ii], alpha=0.33,
+        #         edgecolor=None, linewidth=linewidths[ii], zorder=zorders[ii],
+        #         linestyle=linestyles[ii])
+        # ax_scatter.plot(xmid/1e3, ff_avg, color=colors[ii], label=labels[ii],
+        #     linewidth=lws[ii], linestyle=linestyles[ii],
+        #     zorder=zorders[ii])
+        # ax_scatter.set_ylim(ff_ylim)
+        # ax_scatter.set_yticks(ff_yticks)
+        # ax_scatter.set_xlim([0, 100])
+        # ax_scatter.grid(linestyle=':', linewidth=0.5)
 
-        if ii==0:
-            ax_scatter.set_xlabel('x (km)')
+        # if ii==0:
+        #     ax_scatter.set_xlabel('x (km)')
 
-        # Timeseries
-        for j, xb in enumerate(x_bands):
-            xmin = xb - band_width/2
-            xmax = xb + band_width/2
-            node_mask = np.logical_and(nodes[:, 0]/1e3>=xmin, nodes[:, 0]/1e3<xmax)
-            f_mean = np.sum(ff[node_mask, :]*np.vstack(node_area[node_mask]), axis=0)/np.sum(node_area[node_mask])
-            f_lower = np.quantile(ff[node_mask, :], 0.025, axis=0)
-            f_upper = np.quantile(ff[node_mask, :], 0.975, axis=0)
-            timeax = axs_timeseries[j]
+        # # Timeseries
+        # for j, xb in enumerate(x_bands):
+        #     xmin = xb - band_width/2
+        #     xmax = xb + band_width/2
+        #     node_mask = np.logical_and(nodes[:, 0]/1e3>=xmin, nodes[:, 0]/1e3<xmax)
+        #     f_mean = np.sum(ff[node_mask, :]*np.vstack(node_area[node_mask]), axis=0)/np.sum(node_area[node_mask])
+        #     f_lower = np.quantile(ff[node_mask, :], 0.025, axis=0)
+        #     f_upper = np.quantile(ff[node_mask, :], 0.975, axis=0)
+        #     timeax = axs_timeseries[j]
 
-            if fill_between:
-                timeax.fill_between(tt, f_lower, f_upper, facecolor=colors[ii], alpha=0.3)
+        #     if fill_between:
+        #         timeax.fill_between(tt, f_lower, f_upper, facecolor=colors[ii], alpha=0.3)
 
-            timeax.plot(tt, f_mean, label=labels[ii],
-                color=colors[ii], linewidth=lws[ii], zorder=zorders[ii],
-                linestyle=linestyles[ii])
+        #     timeax.plot(tt, f_mean, label=labels[ii],
+        #         color=colors[ii], linewidth=lws[ii], zorder=zorders[ii],
+        #         linestyle=linestyles[ii])
 
-            mapax.axvline(xb, color='w', linewidth=0.5)
-            timeax.axvline(tslice/365, color='k', linewidth=0.5)
+        #     mapax.axvline(xb, color='w', linewidth=0.5)
+        #     timeax.axvline(tslice/365, color='k', linewidth=0.5)
 
-            timeax.text(0.025, 0.95, time_alphabet[j], transform=timeax.transAxes,
-            va='top', ha='left', **text_args)
+        #     timeax.text(0.025, 0.95, time_alphabet[j], transform=timeax.transAxes,
+        #     va='top', ha='left', **text_args)
 
-            timeax.set_xticklabels([])
-
-
-    ax_scatter.set_ylabel(r'$p_{\rm{w}}/p_{\rm{i}}$')
-    ax_scatter.text(0.95, 0.95, map_alphabet[n_cases], transform=ax_scatter.transAxes,
-        va='top', ha='right', **text_args)
+        #     timeax.set_xticklabels([])
 
 
-    melt = temp_fun(tt)
-    melt_ax = axs_timeseries[-1]
-    melt_ax.plot(tt, melt, color='k', linewidth=1)
+    # ax_scatter.set_ylabel(r'$p_{\rm{w}}/p_{\rm{i}}$')
+    # ax_scatter.text(0.95, 0.95, map_alphabet[n_cases], transform=ax_scatter.transAxes,
+    #     va='top', ha='right', **text_args)
 
-    melt_ax.set_ylabel('Temperature ($^\circ$C)')
-    melt_ax.set_ylim([0, 12])
-    melt_ax.set_yticks([0, 4, 8, 12])
-    melt_ax.grid(linestyle=':', linewidth=0.5)
-    melt_ax.axvline(tslice/365, color='k', linewidth=0.5)
-    melt_ax.text(0.025, 0.95, time_alphabet[j+1], transform=melt_ax.transAxes,
-        va='top', ha='left', **text_args)
 
-    axs_maps[2].set_ylabel('y (km)')
+    # melt = temp_fun(tt)
+    # melt_ax = axs_timeseries[-1]
+    # melt_ax.plot(tt, melt, color='k', linewidth=1)
 
-    cax1 = fig.add_subplot(gs_maps[1:6, 1])
-    cax2 = fig.add_subplot(gs_maps[0, 0])
+    # melt_ax.set_ylabel('Temperature ($^\circ$C)')
+    # melt_ax.set_ylim([0, 12])
+    # melt_ax.set_yticks([0, 4, 8, 12])
+    # melt_ax.grid(linestyle=':', linewidth=0.5)
+    # melt_ax.axvline(tslice/365, color='k', linewidth=0.5)
+    # melt_ax.text(0.025, 0.95, time_alphabet[j+1], transform=melt_ax.transAxes,
+    #     va='top', ha='left', **text_args)
 
-    fig.colorbar(fcolor, cax=cax1, extend='max')
-    fig.colorbar(lc, cax=cax2, orientation='horizontal', extend='both')
+    # axs_maps[2].set_ylabel('y (km)')
 
-    cax1.xaxis.tick_top()
-    cax1.xaxis.set_label_position('top')
+    # cax1 = fig.add_subplot(gs_maps[1:6, 1])
+    # cax2 = fig.add_subplot(gs_maps[0, 0])
 
-    cticks = np.linspace(0, Qmax, 6)
-    cticks[0] = Qmin
-    cticks = np.unique(cticks)
-    print(Qmin)
-    print(Qmax)
-    print(cticks)
-    cax2.set_xticks(cticks)
+    # fig.colorbar(fcolor, cax=cax1, extend='max')
+    # fig.colorbar(lc, cax=cax2, orientation='horizontal', extend='both')
 
-    cax2.xaxis.tick_top()
-    cax2.xaxis.set_label_position('top')
+    # cax1.xaxis.tick_top()
+    # cax1.xaxis.set_label_position('top')
 
-    cax1.text(0, 1.1, r'$p_{\rm{w}}/p_{\rm{i}}$', transform=cax1.transAxes)
-    cax2.set_xlabel(r'$Q~(\rm{m}^3~\rm{s}^{-1})$')
+    # cticks = np.linspace(0, Qmax, 6)
+    # cticks[0] = Qmin
+    # cticks = np.unique(cticks)
+    # print(Qmin)
+    # print(Qmax)
+    # print(cticks)
+    # cax2.set_xticks(cticks)
 
-    axs_timeseries[0].legend(bbox_to_anchor=[0, 1.02, 1., 0.102], loc='lower left',
-        ncol=2, mode='expand', borderaxespad=0.05, frameon=False, borderpad=0)
-    for j, xb in enumerate(x_bands):
-        axi = axs_timeseries[j]
-        axi.set_xticklabels([])
-        axi.set_xlim(t_lim)
-        axi.set_ylim(ff_ylim)
-        axi.set_yticks(ff_yticks)
-        axi.set_xticks(t_ticks)
-        axi.grid(linestyle=':', linewidth=0.5)
+    # cax2.xaxis.tick_top()
+    # cax2.xaxis.set_label_position('top')
 
-        ax_scatter.axvline(xb, color='k', linewidth=0.5)
+    # cax1.text(0, 1.1, r'$p_{\rm{w}}/p_{\rm{i}}$', transform=cax1.transAxes)
+    # cax2.set_xlabel(r'$Q~(\rm{m}^3~\rm{s}^{-1})$')
 
-    melt_ax.set_xlim(t_lim)
-    melt_ax.set_xticks(t_ticks)
-    if t_ticklabels:
-        melt_ax.set_xticklabels(t_ticklabels)
+    # axs_timeseries[0].legend(bbox_to_anchor=[0, 1.02, 1., 0.102], loc='lower left',
+    #     ncol=2, mode='expand', borderaxespad=0.05, frameon=False, borderpad=0)
+    # for j, xb in enumerate(x_bands):
+    #     axi = axs_timeseries[j]
+    #     axi.set_xticklabels([])
+    #     axi.set_xlim(t_lim)
+    #     axi.set_ylim(ff_ylim)
+    #     axi.set_yticks(ff_yticks)
+    #     axi.set_xticks(t_ticks)
+    #     axi.grid(linestyle=':', linewidth=0.5)
 
-    axs_timeseries[-1].set_xlabel(t_xlabel)
-    axs_timeseries[1].set_ylabel(r'$p_{\rm{w}}/p_{\rm{i}}$')
+    #     ax_scatter.axvline(xb, color='k', linewidth=0.5)
+
+    # melt_ax.set_xlim(t_lim)
+    # melt_ax.set_xticks(t_ticks)
+    # if t_ticklabels:
+    #     melt_ax.set_xticklabels(t_ticklabels)
+
+    # axs_timeseries[-1].set_xlabel(t_xlabel)
+    # axs_timeseries[1].set_ylabel(r'$p_{\rm{w}}/p_{\rm{i}}$')
 
     fig.savefig(figname, dpi=300)
     return fig
